ff50bb2b3145347382077f821fe21fd1f3363f7e
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-09-23 20:50:30 +0300

    27 : Classes

---
+++ 27_classes/src/CustomerManager.java
@@ -0,0 +1,15 @@
+public class CustomerManager {
+    
+    public void Add(){
+        System.out.println("Customer added!");
+    }
+
+    public void Delete(){
+        System.out.println("Customer deleted!");
+    }
+
+    public void Update(){
+        System.out.println("Customer updated!");
+    }
+
+}
---
+++ 27_classes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        var customerManager = new CustomerManager();
+
+        customerManager.Add();
+        customerManager.Delete();
+        customerManager.Update();
+
+    }
+}
